cca78f88ed7f711d00d01eb29f32e1df496196b9
Abidyafi49 <abid.yafi.abiyyu.1@gmail.com>
2023-01-18 20:03:16 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+const sidebar = document.querySelector('.sidebar');
+const toggle = document.querySelector('.toggle');
+const searchBtn = document.querySelector('.search-box');
+const modeSwitch = document.querySelector('.toggle-switch');
+const modeText = document.querySelector('.mode-text')
+
+// Event buat darkmode
+modeSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        modeText.innerText = "Light Mode"
+    } else {
+        modeText.innerText = "Dark Mode"
+    }
+})
+
+
+
+// Event hide sidebar
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
+})
+
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove('close')
+})
